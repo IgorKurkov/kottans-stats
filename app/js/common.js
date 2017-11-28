9e0e7a648b4e9a915618459b4c5976a51f0c30a2
kurkov87 <kurkov87@gmail.com>
2017-11-28 21:47:24 +0200

    Add countdown timer

--- app/js/common.js
+++ app/js/common.js
@@ -230,3 +230,77 @@ $('.count').each(function () {
       }
   });
 });
+
+//slickcitcular https://www.jqueryscript.net/demo/Slick-Circular-jQuery-Countdown-Plugin-Classy-Countdown/
+$('#countdown-container').ClassyCountdown({
+  // flat-colors, flat-colors-wide, flat-colors-very-wide, 
+  // flat-colors-black, black, black-wide, black-very-wide, 
+  // black-black, white, white-wide, 
+  // white-very-wide or white-black
+  theme: "white", 
+  
+  // end time
+  end: $.now() + 645600,
+  now: $.now(),
+  
+  // whether to display the days/hours/minutes/seconds labels.
+  labels: true,
+  
+  // object that specifies different language phrases for says/hours/minutes/seconds as well as specific CSS styles.
+  labelsOptions: {
+    lang: {
+      days: 'Days',
+      hours: 'Hours',
+      minutes: 'Minutes',
+      seconds: 'Seconds'
+    },
+    style: 'font-size: 0.5em;'
+  },
+  
+  // custom style for the countdown
+  style: {
+    element: '',
+    labels: false,
+    text: '<a href="https://www.jqueryscript.net/tags.php?/Responsive/">Responsive</a>',
+    // : 0.5,
+    days: {
+      gauge: {
+        thickness: 0.02,
+        bgColor: 'rgba(0, 0, 0, 0)',
+        fgColor: '#1ABC9C',//'rgba(0, 0, 0, 1)',
+        lineCap: 'butt'
+      },
+      textCSS: ''
+    },
+    hours: {
+      gauge: {
+        thickness: 0.02,
+        bgColor: 'rgba(0, 0, 0, 0)',
+        fgColor: '#2980B9',
+        lineCap: 'butt'
+      },
+      textCSS: ''
+    },
+    minutes: {
+      gauge: {
+        thickness: 0.02,
+        bgColor: 'rgba(0, 0, 0, 0)',
+        fgColor: '#8E44AD',
+        lineCap: 'butt'
+      },
+      textCSS: ''
+    },
+    seconds: {
+      gauge: {
+        thickness: 0.02,
+        bgColor: 'rgba(0, 0, 0, 0)',
+        fgColor: '#F39C12',
+        lineCap: 'butt'
+      },
+      textCSS: ''
+    }
+  },
+  
+  // callback that is fired when the countdown reaches 0.
+  onEndCallback: function() {}
+  });

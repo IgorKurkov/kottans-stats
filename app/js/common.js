464fb62789d004af686257add2e9eed5851f8d3e
kurkov87 <kurkov87@gmail.com>
2017-11-28 22:56:31 +0200

    move animate numbers to separate js file

--- app/js/common.js
+++ app/js/common.js
@@ -218,16 +218,5 @@ function drawVerticalBarChart (graphArr) {
   chart.draw(data, options);
   }
 } 
-//https://codepen.io/shivasurya/pen/FatiB
-$('.count').each(function () {
-  $(this).prop('Counter',0).animate({
-      Counter: $(this).text()
-  }, {
-      duration: 1000,
-      easing: 'swing',
-      step: function (now) {
-          $(this).text(Math.ceil(now));
-      }
-  });
-});
+
 

db89218f89cc4e58ae34d44c99e6dd6c47ceaab8
kurkov87 <kurkov87@gmail.com>
2017-11-30 00:52:21 +0200

    small fixes

--- app/js/table.js
+++ app/js/table.js
@@ -8,7 +8,7 @@ function myFunction() {
 
   // Loop through all table rows, and hide those who don't match the search query
   for (i = 0; i < tr.length; i++) {
-    td = tr[i].getElementsByTagName("td")[1];
+    td = tr[i].getElementsByTagName("td")[0];
     if (td) {
       if (td.innerHTML.toUpperCase().indexOf(filter) > -1) {
         tr[i].style.display = "";

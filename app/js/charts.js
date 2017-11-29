e0b1e2ddfc3123de6c37c47f034afea62ebbbbf1
kurkov87 <kurkov87@gmail.com>
2017-11-29 07:40:56 +0200

    test highcharts for activity chart

--- app/js/charts.js
+++ app/js/charts.js
@@ -1,3 +1,54 @@
+var seriesOptions = [],
+seriesCounter = 0,
+names = ['MSFT', 'AAPL', 'GOOG'];
+
+/**
+* Create the chart when all data is loaded
+*/
+function createChart() {
+
+Highcharts.stockChart('container', {
+
+    rangeSelector: {
+        selected: 4
+    },
+
+    plotOptions: {
+        series: {
+            showInNavigator: true
+        }
+    },
+
+    tooltip: {
+        pointFormat: '<span style="color:{series.color}">{series.name}</span>: <b>{point.y} USD</b><br/>',
+        valueDecimals: 2
+    },
+
+    series: seriesOptions
+});
+}
+
+$.each(names, function (i, name) {
+
+  $.getJSON('https://www.highcharts.com/samples/data/jsonp.php?filename=' 
+          + name.toLowerCase() + '-c.json&callback=?', function (data) {
+console.log(data)
+    seriesOptions[i] = {
+        name: name,
+        data: data
+    };
+
+    // As we're loading the data asynchronously, we don't know what order it will arrive. So
+    // we keep a counter and create the chart when all the data is loaded.
+    seriesCounter += 1;
+
+    if (seriesCounter === names.length) {
+        createChart();
+    }
+  });
+});
+
+
 
 // function drawDoubleChart (pieArr, barArr) {
 //   // Load Charts and the corechart and barchart packages.

94b2f78b78d662ff77f8b50a814a4d88e40feb95
Igor Kurkov <kurkov87@gmail.com>
2018-02-19 00:16:51 +0200

    add function for drawing calendar

--- app/js/render/_page-filters.js
+++ app/js/render/_page-filters.js
@@ -1,3 +1,5 @@
+const request        = require('../_request');
+
 const carousel = document.querySelector('.block-date-scroll');
 const messagesContainer = document.querySelector('.center-messages-content');
 
@@ -38,3 +40,87 @@ exports.printMessages = function() {
   }
   messagesContainer.innerHTML = html;
 }
+
+// function formatDate(sent, splitter) {
+//   var dateSentFormatted = sent.getFullYear() +splitter+ 
+//       ("0"+ (sent.getMonth() + 1)).slice(-2) +splitter+ 
+//       ("0"+ sent.getDate()).slice(-2) +" "+ 
+//       ("0"+ sent.getHours()).slice(-2) +":"+ 
+//       ("0"+ sent.getMinutes()).slice(-2);
+//   return dateSentFormatted;
+// }
+
+
+function drawMessages(data) {
+  console.log(data);
+}
+
+
+exports.drawCalendar = function (activityArr) {
+  let buildedArr = [];
+  activityArr.forEach(function(dayObj) {
+    buildedArr.push({
+      "date": dayObj._id,
+      "badge": false,
+      "title": `${dayObj.count} messages`,
+      "classname": `day-block-${(dayObj.count > 100) ? 110 : dayObj.count }`,
+    });
+  });
+console.log(buildedArr)
+
+
+  $(document).ready(function () {
+      $("#my-calendar").zabuto_calendar({
+          action: function () {
+              return myDateFunction(this.id, false);
+          },
+          
+          data: buildedArr, //eventData,
+          modal: false,
+          legend: [
+
+            {type: "text", label: "less 10 messages"},
+            // {type: "spacer"},
+            {type: "list", list: ["day-block-20", "day-block-35", "day-block-45", "day-block-65", "day-block-75", "day-block-95"]},
+            // {type: "spacer"},
+            {type: "text", label: "more 100 messages"},
+            {type: "spacer"},
+          ],
+          cell_border: true,
+          today: true,
+          // show_days: false,
+          // weekstartson: 0,
+          nav_icon: {
+            prev: '<i class="fa fa-chevron-circle-left"></i>',
+            next: '<i class="fa fa-chevron-circle-right"></i>'
+          }
+          
+      });
+      function myDateFunction(id, fromModal) {
+        $("#date-popover").hide();
+        if (fromModal) {
+            $("#" + id + "_modal").modal("hide");
+        }
+        var date = $("#" + id).data("date");
+        var hasEvent = $("#" + id).data("hasEvent");
+        console.log(date)
+
+        request.request("perdate", drawMessages, {
+          method: "POST",
+          body: date
+        });
+
+        if (hasEvent && !fromModal) {
+            return false;
+        }
+        $("#date-popover-content").html('You clicked on date ' + date);
+        $("#date-popover").show();
+        return true;
+      } 
+    
+ });
+ 
+}
+
+
+

a36cb7b184ba2ffc3400ef3d20abeb4b08118724
Igor Kurkov <kurkov87@gmail.com>
2018-02-28 04:27:53 +0200

    add dist folder

--- app/js/plugins/_selectors.js
+++
@@ -1,7 +0,0 @@
-exports.blocks = {
-  messagesCount:    document.querySelector(".count-messages"),
-  starredRepo:      document.querySelector(".starred-repo"),
-  activeUsersCount: document.querySelector(".active-users"),
-  blockLearners:    document.querySelector(".learners"),
-  
-} 
